8d27db73a5b50f056fedef482974c831bbddbbfc
GabrielDS2118 <gabrielestudios2021@gmail.com>
2022-07-20 15:11:47 -0500

    validations_path_parameters

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body,Query
+from fastapi import Body,Query,Path
+
+
 app = FastAPI()
 
 #Models
@@ -40,7 +42,28 @@ def show_person(
     #Opcionalmente recibiremos un str
     #Por defecto será None
     #Si la persona escribe algo,debe ser menor a 50 caracteres y mayor a 1
-    name: Optional[str] = Query(None,min_length=1,max_length=50),
-    age: str = Query(...)
+    
+    name: Optional[str] = Query(
+            None,
+            min_length=1,
+            max_length=50,
+            title = "Person name",
+            description = "This is the person name. It´s between 1 and 50 characters"
+        ),
+    
+    age: int = Query(
+        ...,
+        title = "Person age",
+        description = "This is the person age. It´s required"
+    )
+
 ):
     return {name:age}
+
+
+#3.Validaciones: Path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+        person_id: int = Path(...,gt=0)
+):
+    return {person_id: "it exists!"}
